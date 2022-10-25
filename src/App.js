d4c0a94a5982d1b61883d88b69f6ebf172385c16
LucasNahuel <munozlucas96@gmail.com>
2022-10-25 20:54:36 -0300

    componetized notifications, moved image assets to public/ directory

--- src/App.js
+++ src/App.js
@@ -1,16 +1,86 @@
 import logo from './logo.svg';
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
 import './App.css';
 
-import avatarMarkWebber from './assets/images/avatar-mark-webber.webp';
-import avatarAngelaGray from './assets/images/avatar-angela-gray.webp';
-import avatarJacobThompson from './assets/images/avatar-jacob-thompson.webp';
-import avatarRizkyHasanuddin from './assets/images/avatar-rizky-hasanuddin.webp';
-import avatarKimberlySmith from './assets/images/avatar-kimberly-smith.webp';
-import avatarNathanPeterson from './assets/images/avatar-nathan-peterson.webp';
-import avatarAnnaKim from './assets/images/avatar-anna-kim.webp';
-import imageChess from './assets/images/image-chess.webp';
+import notificationsData from './assets/data/notifications.json';
+
+
+class Notification extends React.Component {
+
+  constructor(props){
+    super(props);
+    console.log(props);
+  }
+
+
+  render(){
+
+    return(
+    <li className='notif-root'>
+      <div className='notif-picture-li'>
+          <div className='notif-picture-container'>
+            <img className='notification-profile-pic' src={this.props.value.userProfileImgSrc}></img>
+            <div>
+              <p className='notification-description'>
+                <a>{this.props.value.user}</a>
+                {'  '+this.props.value.action+'  '}
+                <a>{this.attachTarget()}</a>
+                {this.showUnreadPoint()}
+              </p>
+              <div>{this.props.value.since}</div>
+              {this.attachMessagePreview()}
+            </div>
+          </div>
+          
+
+          {this.attachPicture()}
+
+        </div>
+          
+          
+        </li>
+    )
+  }
+
+  attachPicture(){
+    if(this.props.value.relatedImgSrc){
+      return <div className='notif-picture'>
+              <img className='notification-profile-pic' src='/images/image-chess.webp'></img>
+            </div>
+    }
+  }
+
+  attachTarget(){
+    if(this.props.value.target){
+      return <a className='notif-post-link'>{this.props.value.target}</a>
+    }
+  }
+
+  showUnreadPoint(){
+    if(this.props.value.read === "false"){
+      return <div className='notification-point'></div>
+    }
+
+  }
+
+  attachMessagePreview(){
+    if(this.props.value.messageAttached){
+      return <div className='notification-message-preview'>
+
+        <p>
+          {this.props.value.messageAttached} 
+        </p>
+
+      </div>
+    }
+  }
+
+}
 
 function App() {
+
   return (
     <div id='root-div'>
       <div className='notification-page-header'>
@@ -25,119 +95,25 @@ function App() {
         
       </div>
       <ul className='notification-list'>
-        <li>
-          <img className='notification-profile-pic' src={avatarMarkWebber}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Mark Webber</a>
-              {' reacted to your recent post '}
-              <a className='notif-post-link'>My first tournament today!</a>
-              <div className='notification-point'></div>
-            </p>
-            <div>1m ago</div>
-          </div>
-          
-          
-        </li>
-        <li>
-          <img className='notification-profile-pic' src={avatarAngelaGray}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Angela Gray</a>
-              {' followed you '}
-              <div className='notification-point'></div>
-            </p>
-            <div>5 m ago</div>
-          </div>
-          
-          
-        </li>
-        <li>
-          <img className='notification-profile-pic' src={avatarJacobThompson}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Jacob Thompson</a>
-              {' has joined your group '}
-              <a className='notif-group-link'>Chess Club</a>
-              <div className='notification-point'></div>
-            </p>
-            <div>1 day ago</div>
-          </div>
-          
-          
-        </li>
 
-        <li>
-          <img className='notification-profile-pic' src={avatarRizkyHasanuddin}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Rizky Hasanuddin</a>
-              {' sent you a private message '}
+        {printNotifications()}
 
-            </p>
-            <div>5 days ago</div>
-            <div className='notification-message-preview'>
-
-              <p>
-                Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and i'm already having lost of fun and improving my game.
-              </p>
-
-            </div>
-          </div>
-          
-          
-        </li>
-
-        <li className='notif-picture-li'>
-          <div className='notif-picture-container'>
-            <img className='notification-profile-pic' src={avatarKimberlySmith}></img>
-            <div>
-              <p className='notification-description'>
-                <a>Kimberly Smith</a>
-                {' commented on your picture '}
-              </p>
-              <div>1 week ago</div>
-            </div>
-          </div>
-          
-
-          <div className='notif-picture'>
-            <img className='notification-profile-pic' src={imageChess}></img>
-          </div>
-          
-          
-        </li>
-
-        <li>
-          <img className='notification-profile-pic' src={avatarNathanPeterson}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Nathan Peterson</a>
-              {' reacted to your recent post '}
-              <a className='notif-post-link'>5 end-game strategies to increase your win rate</a>
-            </p>
-            <div>2 weeks ago</div>
-          </div>
-          
-          
-        </li>
-
-        <li>
-          <img className='notification-profile-pic' src={avatarAnnaKim}></img>
-          <div>
-            <p className='notification-description'>
-              <a>Anna Kim</a>
-              {' left the group '}
-              <a className='notif-group-link'>Chess Club</a>
-            </p>
-            <div>2 weeks ago</div>
-          </div>
-          
-          
-        </li>
       </ul>
     </div>
   );
 }
+
+function printNotifications(){
+  let i = 0;
+  let notifs = [];
+  while(i < notificationsData.length){
+    notifs.push(<Notification value={notificationsData[i]}></Notification>);
+    i= i+1;
+  }
+  return notifs;
+}
+
+
+
 
 export default App;
